406426753e6e79d7620b32f9c94f33743be9a70e
Ayobami Ayodeji <ayodejiayobami99@gmail.com>
2023-01-25 22:13:42 +0100

    Update 105-radix_sort.c

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,56 +1,73 @@
 #include "sort.h"
 
 /**
- * radix_sort - sorts an array following the Radix sort algorithm
- * @array: array of ints to sort
- * @size: size of the array
- */
-void radix_sort(int *array, size_t size)
+* findmax - Finds the maximum value in an array
+* @array: array to find max value of
+* @size: Size of array
+* Return: Largest value
+*/
+
+int findmax(int *array, size_t size)
 {
-	int max;
-	size_t i, lsd;
+	int i, max = 0;
 
-	if (!array || size < 2)
-		return;
-
-	max = 0;
-	for (i = 0; i < size; i++)
-		if (array[i] > max)
-			max = array[i];
-
-	for (lsd = 1; max / lsd > 0; lsd *= 10)
+	for (i = 0; i < (int)size; i++)
 	{
-		count_sort_LSD(array, size, lsd);
-		print_array(array, size);
+		if (max < array[i])
+			max = array[i];
 	}
+	return (max);
 }
 
 /**
- * count_sort_LSD - count sort with LSD
- * @array: array to sort
- * @size: size of the array
- * @lsd: least significant digit
- */
-void count_sort_LSD(int *array, size_t size, size_t lsd)
+* radix_sort - Sorts an array using radix sort algo
+* @array: Array to sort
+* @size: size of array
+*/
+
+void radix_sort(int *array, size_t size)
 {
-	int count_arr[10] = {0}, *out_arr, l, m;
-	size_t k, n;
+	int m, pos, *out, *ca;
 
-	out_arr = malloc(sizeof(int) * size);
+	if (array == NULL || size < 2)
+		return;
+	m = findmax(array, size);
+	out = malloc(sizeof(int) * (int)size);
+	ca = malloc(sizeof(int) * (10));
+	if (ca == NULL || out == NULL)
+		return;
+	for (pos = 1; m / pos > 0; pos *= 10)
+		counting_sort_r(array, size, pos, out, ca), print_array(array, size);
+	free(out);
+	free(ca);
+}
 
-	for (k = 0; k < size; k++)
-		count_arr[(array[k] / lsd) % 10]++;
-	for (l = 1; l < 10; l++)
-		count_arr[l] += count_arr[l - 1];
+/**
+* counting_sort_r - sorts array using counting algorithm
+* @array: Array to sort
+* @size: Size of array
+* @pos: Digit position value
+* @out: Temp output array
+* @ca: Count array
+*/
 
-	for (m = size - 1; m >= 0; m--)
+void counting_sort_r(int *array, size_t size, int pos, int *out, int *ca)
+{
+	int i;
+
+	if (array == NULL || size < 2)
+		return;
+	for (i = 0; i < 10; i++)
+		ca[i] = 0;
+	for (i = 0; i < (int)size; i++)
+		ca[((array[i] / pos) % 10)] += 1;
+	for (i = 0; i < 10; i++)
+		ca[i] += ca[i - 1];
+	for (i = size - 1; i >= 0; i--)
 	{
-		out_arr[count_arr[(array[m] / lsd) % 10] - 1] = array[m];
-		count_arr[(array[m] / lsd) % 10]--;
+		out[ca[((array[i] / pos) % 10)] - 1] = array[i];
+		ca[((array[i] / pos) % 10)] -= 1;
 	}
-
-	for (n = 0; n < size; n++)
-		array[n] = out_arr[n];
-
-	free(out_arr);
+	for (i = 0; i < (int)size; i++)
+		array[i] = out[i];
 }
